9a1484f94b6d1e0d49a7b68cf406836f390ee110
bveldhuyzen <77223171+bveldhuyzen@users.noreply.github.com>
2021-12-24 03:35:54 +0000

    Add files via upload

---
+++ rounding-down-module-v102-for-lists/read_menu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#echo '__________________________________________________________'
+#echo 'enter directory of list file'
+#read list_directory
+#cd "$list_directory"
+echo '__________________________________________________________'
+echo 'ls'
+ls
+echo '__________________________________________________________'
+echo ''
+echo 'type list filename'
+read list_filename
+
+
+echo 'round_to_this'
+read round_to_this 
+
+
+read_menu
